cbb626d7197ca3cae34b2b359c12d4382d360b48
Labani4321 <157562462+Labani4321@users.noreply.github.com>
2024-09-17 11:31:45 +0530

    ATM.py

--- ATM.py
+++ ATM.py
@@ -1,45 +1,45 @@
 import time 
+
 print("insert your ATM pin ")
 password = 1234
-pin = int (input("enter your ATM pin :"))
+pin = int(input("enter your ATM pin: "))
 
-
-if pin==password:
+if pin == password:
     balance = 50000
     
-    while True :
+    while True:
         print('''
-    1 == balance
-    2 == withdraw cash
-    3 == deposite balance
-    4 == exit
-    
+    1 == Check balance
+    2 == Withdraw cash
+    3 == Deposit balance
+    4 == Exit
     ''')
 
-    try: 
-        option =int (input("enter your option:" ))
-    except ValueError: 
-        print(f"Please enter your valid option :")
-    
-    if option==1 :
-        print (f"your current balance is {balance}")
-    if option ==2:
-        withdraw_amount = int(input("please enter the withdraw_amount: "))
-        balance =balance-withdraw_amount
-        print (f"{withdraw_amount} is debited from your account")
-        print (f"your current balance is {balance}")
+        try: 
+            option = int(input("enter your option: "))
+        except ValueError: 
+            print("Please enter a valid option.")
+            continue  
 
-    if option ==3:
-        deposit_amount = int(input("please enter your amount"))
-        balance= balance+deposit_amount
-        print(f"{deposit_amount} is credited to your account")
-        print(f"your current balance is {balance}")
-    if option ==4:
-          print("existing the system")
-          
-    else:
-        print("wrong pin please try again")
-else :
-    print("wrong pin.please try again")
-
-
+        if option == 1:
+            print(f"Your current balance is {balance}")
+        elif option == 2:
+            withdraw_amount = int(input("Please enter the withdrawal amount: "))
+            if withdraw_amount <= balance:
+                balance -= withdraw_amount
+                print(f"{withdraw_amount} is debited from your account.")
+                print(f"Your current balance is {balance}")
+            else:
+                print("Insufficient balance!")
+        elif option == 3:
+            deposit_amount = int(input("Please enter your deposit amount: "))
+            balance += deposit_amount
+            print(f"{deposit_amount} is credited to your account.")
+            print(f"Your current balance is {balance}")
+        elif option == 4:
+            print("Exiting the system.")
+            break  
+        else:
+            print("Invalid option. Please try again.")
+else:
+    print("Wrong pin. Please try again.")
