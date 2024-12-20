773d5ae7ade3f41020617d0ee56e9c8cdf1c785b
Labani Haldar <157562462+Labani4321@users.noreply.github.com>
2024-12-20 11:25:40 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,28 @@
+a= [1,2,3,4,5,6]
+print (a)
+print(a[2])
+a[1]=98
+print(a)
+c=[45,"labani","pinki",6.9,"rituja"]
+print(c)
+freinds= ["labani","pinki","ishita","rituja"]
+print(freinds[0:3])
+print(freinds[-2:])
+t=[1,3,4,0,5,8,6,9,7,10]
+print(t)
+t.sort()
+print(t)
+t.reverse()
+t.sort()
+t.insert(1,457)
+t.append(58)
+t.sort()
+t.pop(2)
+t.remove(0)
+print(t)
+s=(14,56,35,40,14,14)
+s1=(1,2,)
+print(s1)
+w=(14,56,35,40,14,14)
+print(s.count(14))
+print(s.index(56))
