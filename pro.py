2caf2ac812d2b2410236fdf6247819f89a62b8fb
Labani Haldar <157562462+Labani4321@users.noreply.github.com>
2024-12-20 10:33:12 +0530

    Add files via upload

---
+++ pro.py
@@ -0,0 +1,16 @@
+class Book :
+    def __init__(self,title,author,year_published):
+        self.title = title
+        self.author = author
+        self.year_published = year_published
+
+    def print_details(self):
+        print(f"Title:{self.title}")
+        print(f"Author:{self.author}")
+        print(f"year published:{self.year_published}")
+
+book = Book("The girl with no name","Marina Chapman", 2013)
+book.print_details()
+
+
+
