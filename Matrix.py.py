812f074f000ccf2db1e9f7354edb6798dca1fbfe
Labani Haldar <157562462+Labani4321@users.noreply.github.com>
2025-02-12 23:18:19 +0530

    Add files via upload

---
+++ Matrix.py.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Create a 2x2 array of zeros
+b = np.zeros((2, 2))
+print("Matrix b:", b)
+
+# Create a 3x3 array of zeros
+a = np.zeros((3, 3))
+print("Matrix a:", a)
+
+# Create a 3x3 array of zeros with float data type
+c = np.zeros((3, 3), dtype=float)
+print("Matrix c:", c)
+n = np.ones ((3,2))
+print (n)
+p= np.full((4,4),34,dtype=float)
+print (p)
