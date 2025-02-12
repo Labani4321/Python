3259e86c1c96063d359cd2ad4b1d31bd375ff0b1
Labani Haldar <157562462+Labani4321@users.noreply.github.com>
2025-02-12 23:23:36 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+MyDictionary={
+    "Name" :"labani",
+    "father name " : "Prodip",
+    "mother name ":"lalita",
+    "marks": "[1,2,3]"
+
+}
+print(MyDictionary ['Name'])
+print(MyDictionary['father name '])
+print(MyDictionary['mother name '])
+
+
